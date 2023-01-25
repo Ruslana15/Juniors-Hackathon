4bc66ef1e1ae05e73485b42f415663f421388cb3
userbaeff <userbaeff@gmail.com>
2023-01-26 01:19:54 +0600

    fixed urls for swagger

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+import account
+from account import urls
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -26,5 +28,6 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('accounts/', include(account.urls)),
 ]
 
